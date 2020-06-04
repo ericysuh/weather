402ea4dd2a177a44e1c33f060a332376b3c7a93c
Eric Suh <yoonjjsuh@gmail.com>
2020-06-04 09:23:20 -0400

    add static dailyWeather info to the Accordion

--- src/components/DailyWeather/DailyWeather.js
+++ src/components/DailyWeather/DailyWeather.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-// import { convertEpochToDay } from '../../utilities/timeUtils';
+import { convertEpochToDay } from '../../utilities/timeUtils';
+import { kelvinToFahrenheit, degToCompass } from '../../utilities/weatherUtils';
 import { Accordion } from './Accordion/Accordion';
 import Header from './Accordion/Header/Header';
 import Panel from './Accordion/Panel/Panel';
@@ -12,22 +13,35 @@ const mapState = (state) => ({
   DailyWeatherData: state.weatherData.daily
 });
 
-// const day = (index === 0) ? `${convertEpochToDay(data.dt)} Today` : convertEpochToDay(data.dt);
-
 const DailyWeather = ({ DailyWeatherData }) => (
   <ul className="daily-weather">
-    {DailyWeatherData.map((data) => (
-      <li key={data.dt} className="daily-weather__list">
-        <Accordion>
-          <Header>
-            Heading 1
-          </Header>
-          <Panel>
-            info for panel
-          </Panel>
-        </Accordion>
-      </li>
-    ))}
+    {DailyWeatherData.map((data, index) => {
+      const day = (index === 0) ? `${convertEpochToDay(data.dt)} Today` : convertEpochToDay(data.dt);
+      return (
+        <li key={data.dt} className="daily-weather__list">
+          <Accordion>
+            <Header>
+              <span>{day}</span>
+              <span>
+                {kelvinToFahrenheit(data.temp.max)}&deg;/
+                {kelvinToFahrenheit(data.temp.min)}&deg;
+              </span>
+              <div>
+                <img
+                  src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                  alt={data.weather[0].description}
+                />
+                <span>{data.weather[0].description}</span>
+              </div>
+              <span>{degToCompass(data.wind_deg)} {Math.round(data.wind_speed)} mph</span>
+            </Header>
+            <Panel>
+              info for panel
+            </Panel>
+          </Accordion>
+        </li>
+      );
+    })};
   </ul>
 );
 
