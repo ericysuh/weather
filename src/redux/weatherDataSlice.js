7171f1c0a01a66f1f3835ebfaa0a76a8ca89b2cb
Eric Suh <yoonjjsuh@gmail.com>
2020-06-24 19:55:10 -0400

    Add initial setup for error handling and loading state

--- src/redux/weatherDataSlice.js
+++ src/redux/weatherDataSlice.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { weatherAPI } from '../constants/weatherConstants';
+import handleErrors from '../utilities/fetch';
 
 const updateTempData = (data) => {
-  // update current weather
+  // Update current weather
   data.current.temp = Math.round(data.current.temp);
   data.current.feels_like = Math.round(data.current.feels_like);
-  // update daily weather
+  // Update daily weather
   data.daily = data.daily.slice(1, 8);
   data.daily = data.daily.map((day) => {
     day.temp.min = Math.round(day.temp.min);
@@ -26,44 +27,60 @@ const updateTempData = (data) => {
 const fetchOneCallApi = async ({ lat, lon }) => {
   try {
     const apiURL = `${weatherAPI.baseURL}/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=imperial&appid=${weatherAPI.key}`;
-    const data = await fetch(apiURL).then((res) => res.json());
+    const data = await fetch(apiURL)
+      .then((res) => handleErrors(res))
+      .then((res) => res.json());
+
     return data;
   } catch (err) {
-    throw new Error(err);
+    throw Error(err);
   }
 };
 
 const fetchCurrentWeatherData = createAsyncThunk(
   'weatherData/fetchCurrentWeather',
-  // eslint-disable-next-line consistent-return
   async (zipcode) => {
     try {
       const apiURL = `${weatherAPI.baseURL}/weather?zip=${zipcode},us&appid=${weatherAPI.key}`;
       // Get current weather
-      const data = await fetch(apiURL).then((res) => res.json());
+      const data = await fetch(apiURL)
+        .then((res) => handleErrors(res))
+        .then((res) => res.json());
+
       const { coord, name } = data;
 
       // Get current/hourly/daily weather
       const fullData = await fetchOneCallApi(coord);
       fullData.city = name;
-
       updateTempData(fullData);
 
       return fullData;
     } catch (err) {
-      return err;
+      throw Error(err);
     }
   }
 );
 
-const weatherData = createSlice({
+const weatherDataSlice = createSlice({
   name: 'weatherData',
   initialState: {},
   reducers: {},
   extraReducers: {
-    [fetchCurrentWeatherData.fulfilled]: (state, action) => action.payload
+    [fetchCurrentWeatherData.pending]: (state) => {
+      state.isLoading = true;
+      state.error = {};
+    },
+    [fetchCurrentWeatherData.fulfilled]: (state, action) => {
+      action.payload.isLoading = false;
+      action.payload.error = {};
+      return action.payload;
+    },
+    [fetchCurrentWeatherData.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.error;
+    }
   }
 });
 
-export default weatherData.reducer;
+export default weatherDataSlice.reducer;
 export { fetchCurrentWeatherData };
